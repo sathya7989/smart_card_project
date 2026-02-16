eafde7ea9ccd85c54961f2bd9b168edeba3a3a34
kalyanramnadhagiri <kr2235520@gmail.com>
2026-02-16 10:43:15 +0530

    kalyan is committed

--- 001_smart_card_application/src/main/java/com/sathya/Application.java
+++ 001_smart_card_application/src/main/java/com/sathya/Application.java
@@ -9,6 +9,7 @@ public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 		System.out.println("sathya was initiated");
+		System.out.println("Kalyan Is Intiaited");
 	}
 
 }
